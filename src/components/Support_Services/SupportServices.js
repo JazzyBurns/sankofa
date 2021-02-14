9372e6b23b82d2d514da6969ba899eb5f8bd0317
Ronvic Ernest Cuevas <ronvicernestcuevas@Ronvics-Air.hitronhub.home>
2021-02-14 14:33:08 -0800

    commit on main

---
+++ src/components/Support_Services/SupportServices.js
@@ -0,0 +1,20 @@
+import './Supportservices.css'
+import { Link } from 'react-router-dom';
+function SupportServices()
+{
+    return
+    (
+        <div className= "SupportServices-container">
+            <h2>Support Services Resources</h2>
+            <div className="SupportServices-body">
+                <h5>Click to jump to section</h5>
+                <p className="WR">Website Resources</p>
+                <p className="SLWR">Speech/Language Website Resources</p>
+                <p className="PTWR">Physical Therapy Website Resources</p>
+            </div>
+            
+        </div>
+
+    );
+}
+export default SupportServices
