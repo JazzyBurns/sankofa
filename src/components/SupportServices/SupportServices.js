50968df969bb12d165599005bc122a5a37f28bb8
Ronvic Ernest Cuevas <ronvicernestcuevas@Ronvics-Air.hitronhub.home>
2021-02-14 16:22:18 -0800

    Update SupportServices.js

--- src/components/SupportServices/SupportServices.js
+++ src/components/SupportServices/SupportServices.js
@@ -3,10 +3,11 @@ import './SupportServices.css'
 
 function SupportServices()
 {
-    return
-    (
+    
+   
         <div className= "SupportServices-container">
             <h2>Support Services Resources</h2>
+            
             <div className="SupportServices-body">
                 <h5>Click to jump to section</h5>
                 <p className="WR">Website Resources</p>
@@ -14,8 +15,19 @@ function SupportServices()
                 <p className="PTWR">Physical Therapy Website Resources</p>
             </div>
             
+            <div>
+            <h2>Website Resources</h2>
+            <div className="WebsiteContainer">
+                <ul>
+                    <li>Warmline</li>
+                    <li></li>
+                    
+                </ul>
+                </div>
+                </div>
+            
+        
         </div>
-
-    );
+    
 }
 export default SupportServices;
