4eaafc34feb702000c269ebedf1b287ae5306350
Nandhini <nandhini.sridhar02@gmail.com>
2021-02-20 14:11:52 -0800

    fixed styling- used flexbox

--- src/components/Education/Edu.js
+++ src/components/Education/Edu.js
@@ -5,31 +5,37 @@ import './Education.css';
 function Edu() {
   return (
     <div className="Edu">
-      <h1 className='.eduh1'>Educational Resources</h1>
-      <h2 className='.eduh2'>General Resources</h2>
+      <div className="wrapper">
+      <h1 className="eduh1">Educational Resources</h1>
+      <h2 className="eduh2">General Resources</h2>
 
-      <div className="btnRow">
+      <div className="flexRow">
       <a href="https://www.understood.org">
-        <button className="buttonCol" className='edubutton'>Understood</button>
+        <button className="edubutton">Understood</button>
       </a> 
       <a href="https://sites.ed.gov/idea/">
-        <button className="buttonCol" className='edubutton'>Individuals with Disabilities Education Act</button>
+        <button className="edubutton">Individuals with Disabilities Education Act</button>
       </a> 
       </div>
+      
 
-      <h2 className='.eduh2'>Community Advisory Committees (CACs)</h2>
+      <h2 className="eduh2">Community Advisory Committees (CACs)</h2>
+      <div className="flexCol">
       <a href="https://www.scusd.edu/community-advisory-committee-cac">
-        <button className="btnRow">Sacramento City Unified School District CAC</button>
+        <button className="edubutton" >Sacramento City Unified School District CAC</button>
       </a>
       <a href="https://www.sjcoe.org/selpa/CAC.aspx#:~:text=The%20purpose%20of%20the%20Community,of%20children%20with%20exceptional%20needs">
-        <button className="btnRow">San Joaquin County Office of Education CAC</button>
+        <button className="edubutton">San Joaquin County Office of Education CAC</button>
       </a>
       <a href="https://natomasunified.org/departments/student-services-and-support/community-advisory-committee-cac/">
-        <button className="btnRow">Natomas Unified School District CAC</button>
+        <button className="edubutton">Natomas Unified School District CAC</button>
       </a>
       <a href="https://www.sonomaselpa.org/pub/selpa_docs/cac.html">
-        <button className="btnRow">Sonoma County CAC</button>
+        <button className="edubutton">Sonoma County CAC</button>
       </a>
+      </div>
+
+      </div>
     </div>
   );
 }
