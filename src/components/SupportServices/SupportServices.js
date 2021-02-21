20179a69f0064d5de35e97c7eeb7a6db4e82b5a6
Ronvic Ernest Cuevas <ronvicernestcuevas@Ronvics-Air.hitronhub.home>
2021-02-21 12:49:45 -0800

    fixed margins, need to work on mobile

--- src/components/SupportServices/SupportServices.js
+++ src/components/SupportServices/SupportServices.js
@@ -5,7 +5,7 @@ function SupportServices()
 {
 
    return (
-  <div classname="Support">
+  <div classname="Support-container">
         <h2>Support Services Resources</h2>
     <div className="SupportServices-body">
         
@@ -21,101 +21,113 @@ function SupportServices()
 
     
         
-        <div classname="WebsiteSections">
+        
         
         <h2>Website Resources</h2>
+        <div classname="flexrow">
         <a href="https://health.ucdavis.edu/mindinstitute/centers/cedd.html/"> 
-            <button className="btnCol" className='Webbutton'>Center for Excellence in Developmental Disabilities</button>
+            <button  className='Webbutton'>Center for Excellence in Developmental Disabilities</button>
             
             </a>
            
           <a href="https://thegiftsfoundation.org">
               
-              <button className="btnCol" className='Webbutton'>The Gifts Foundation</button>
+              <button className='Webbutton'>The Gifts Foundation</button>
 
               </a>
+        </div>
 
+        <div classname="flexrow2">
             <a href="http://vistachildtherapy.com">
                 
-                <button className="btnCol" className='Webbutton'>Occupational Therapy</button>
+                <button  className='Webbutton'>Occupational Therapy</button>
                 
                 </a>
 
 
                 <a href="http://www.warmlinefrc.org">
             
-             <button className="btnCol" className='Webbutton'>Warmline</button>
+             <button  className='Webbutton'>Warmline</button>
            
            </a>
            
             
         </div>
 
+     
+
    
 
    
     
-    <div classname="SpeechSections">
+    
         
     <h2>Speech/Language Website Resources</h2>
+    <div classname="flexCol">
         <a href="https://www.asha.org">
-            <button className="btnCol" className='Speechbutton'>American-Speech-Lanugage-Hearing Association (ASHA)</button>
+            <button  className='Speechbutton'>American-Speech-Lanugage-Hearing Association (ASHA)</button>
             </a>
         <a href="http://www.arsgoldriver.com">
-            <button className="btnCol" className='Speechbutton'>American River Speech Therapy</button>
+            <button  className='Speechbutton'>American River Speech Therapy</button>
             </a>
+            </div>
+            <div classname="flexCol2">
         <a href="https://capitolspeechandrehab.com">
-            <button className="btnCol" className='Speechbutton'>Capitol Speech & Rehab</button></a>
+            <button className='Speechbutton'>Capitol Speech & Rehab</button></a>
        <a href="http://cookspeechandlanguage.com/">
-           <button className="btnCol" className='Speechbutton'>Cook Speech and Language</button>
+           <button className='Speechbutton'>Cook Speech and Language</button>
        </a>
        <a href="https://www.ssrlc.org/">
-           <button className="btnCol" className='Speechbutton'>Sacramento Scottish Rite</button>
+           <button  className='Speechbutton'>Sacramento Scottish Rite</button>
            </a>
        <a href="https://tlcinctherapies.com/">
-           <button className="btnCol" className='Speechbutton'>Therapeutic Language Clinic</button>
+           <button  className='Speechbutton'>Therapeutic Language Clinic</button>
            </a>
-    </div>
+
+
+           </div>
+  
    
 
        
             
-            <div classname="Ptsections">
+            
                 <h2>Physical Therapy Website Resources</h2>
+                <div classname="flexcol3">
                 <a href="http://www.babystepstherapy.com">
                     
-                    <button className="btnCol" className='PTbutton'>Baby Steps Therapy</button>
+                    <button  className='PTbutton'>Baby Steps Therapy</button>
 
                 </a>
                 <a href="http://www.burgerrehab.com">
                     
-                    <button className="btnCol" className='PTbutton'>Burger Physical Therapy</button>
+                    <button  className='PTbutton'>Burger Physical Therapy</button>
                     
                 </a>
                 <a href="https://www.ctchelpskids.org">
                    
-                    <button className="btnCol" className='PTbutton'>Children's Therapy Center</button>
+                    <button  className='PTbutton'>Children's Therapy Center</button>
                    
                     </a>
                 <a href="https://www.easterseals.com/superior-ca/">
                    
-                    <button className="btnCol" className='PTbutton'>Easterseals Superior California</button>
+                    <button  className='PTbutton'>Easterseals Superior California</button>
                    
                     </a>
                     
                 <a href="https://www.jabbergym.com/">
                    
-                    <button className="btnCol" className='PTbutton'>Jabber Gym</button>
+                    <button  className='PTbutton'>Jabber Gym</button>
                    
                     </a>
                 <a href="https://www.tesidea.com">
                    
-                    <button className="btnCol" className='PTbutton'>Total Education Solutions</button>
+                    <button  className='PTbutton'>Total Education Solutions</button>
                    
                     </a>
                 <a href="http://www.sierrapediatrics.com">
                     
-                    <button className="btnCol" className='PTbutton'>Sierra Pediatric Therapy Clinic</button>
+                    <button  className='PTbutton'>Sierra Pediatric Therapy Clinic</button>
                     
                     </a>
 
