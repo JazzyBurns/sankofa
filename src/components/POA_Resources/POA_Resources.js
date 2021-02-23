327dc8ab9126a708c3a4e757928620ad9a3fd5d2
ferica <67454164+ferica@users.noreply.github.com>
2021-02-22 18:22:28 -0800

    Update POA_Resources.js

--- src/components/POA_Resources/POA_Resources.js
+++ src/components/POA_Resources/POA_Resources.js
@@ -14,15 +14,15 @@ function POA_Resources() {
 
             <div className="column-container">
             <a href="https://www.aucd.org//template/index.cfm">
-            <button className="rectangle1" >Association of University Centers on Disabilities (AUCD)</button>
+            <button className="rectangle1">Association of University Centers on Disabilities (AUCD)</button>
             </a> 
 
             <a href="https://www.aucd.org/emergingleaders/home">
-            <button className="rectangle2" >ACUD Emerging Leaders Community</button>
+            <button className="rectangle2">ACUD Emerging Leaders Community</button>
             </a> 
 
             <a href="https://www.aapf.org/">
-            <button className="rectangle3" >The African American Policy Forum (AAPF)</button>
+            <button className="rectangle3">The African American Policy Forum (AAPF)</button>
             </a> 
             </div>
 
