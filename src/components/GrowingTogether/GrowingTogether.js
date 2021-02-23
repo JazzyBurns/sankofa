3aa60e30c19f3505f7d9a85b6cf77e5c334f88e8
Clare <cdltran@ucdavis.edu>
2021-02-22 22:48:00 -0800

    Made page more responsive

--- src/components/GrowingTogether/GrowingTogether.js
+++ src/components/GrowingTogether/GrowingTogether.js
@@ -2,19 +2,25 @@ import './GrowingTogether.css';
 
 function GrowingTogether(){
     return (
-        <div className="WholePage">
-            <div className="GrowingTogether">Growing Together</div>
-            <div className ="HowToHelp">How can you help?</div>
-            <div className ="One">01</div>
-            <div className ="Partner">
-            PARTNER: Share time, expertise, information, and resources with 
-            Sankofa facilitators and potentially share with the group.
-            </div>
-            <div className ="Two">02</div>
-            <div className = "Share">
-            SHARE: Help distribute the word about our group and be an ambassador
-            as to the importance of having culturally relevant support groups for
-            families in underserved communities. 
+        <div className = "WholePage">
+            <h1>Growing Together</h1>
+            <p className = "HowToHelp">How can you help?</p>
+            <div className = "Container">
+                <div className = "RowOne">
+                    <h2 className = "One">01</h2>
+                    <p className = "Partner">
+                        PARTNER: Share time, expertise, information, and resources with 
+                        Sankofa facilitators and potentially share with the group.
+                    </p>
+                </div>
+                <div className = "RowTwo">
+                    <h2 className = "Two">02</h2>
+                    <p className = "Share">
+                        SHARE: Help distribute the word about our group and be an ambassador
+                        as to the importance of having culturally relevant support groups for
+                        families in underserved communities. 
+                    </p>
+                </div>  
             </div>
         </div>
     );
