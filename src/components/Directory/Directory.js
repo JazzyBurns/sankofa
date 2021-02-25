34f6901a75c7000076cd2e9f31798d441f70aafb
juliaalberto <56199001+juliaalberto@users.noreply.github.com>
2021-02-25 09:50:25 -0800

    Adding padding and font change to Direct

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -6,7 +6,7 @@ function Direct() {
   return (
     <div className = "direct">
       <div className ="direct-container-title">
-      <h1 className ="direct-h1">Sankofa National Family Respource Directory</h1>
+      <h1>Sankofa National Family Respource Directory</h1>
       </div>
 
       <div className ="direct-container1">
@@ -25,11 +25,11 @@ function Direct() {
       </p>
       </div>
       
-        <div className = "direct-container3">
+        <button className = "direct-button">
         <a href="https://forms.gle/qYXbL3BdFATwwG6y8">
           <button className="direct-button-text"> Join Us </button>
         </a> 
-      </div>
+      </button>
     </div>
 
   );
