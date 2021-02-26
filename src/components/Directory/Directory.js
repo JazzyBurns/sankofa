b2d9f9f6d2ff764a22f68b51a8cc693fb75a7f96
juliaalberto <56199001+juliaalberto@users.noreply.github.com>
2021-02-25 19:57:47 -0800

    Fixed the color background and header

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,12 +1,18 @@
 //import logo from './logo.svg';
 //import dispHeader from './edu-rsc.js';
+import {Helmet} from 'react-helmet'
 import './Directory.css';
 
 function Direct() {
   return (
     <div className = "direct">
+      <Helmet>
+        <title>Sankofa | Directory</title>
+        <style>{'body { background-color: #FFFCF8; }'}</style>
+      </Helmet>
+
       <div className ="direct-container-title">
-      <h1>Sankofa National Family Respource Directory</h1>
+      <h1>Sankofa National Family Response Directory</h1>
       </div>
 
       <div className ="direct-container1">
