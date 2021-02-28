0112df48256b79b60e2f73d2fcca67dad597d43e
allisonwchan <allison.wl.chan@gmail.com>
2021-02-27 17:57:11 -0800

    added react helmet to fix background color, installed emailjs to handle backend

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -2,10 +2,32 @@ import './ContactUs.css';
 import mail from './mailicon.png'
 import phone from './phoneicon.png'
 import fb from './facebookicon.png'
+import emailjs from 'emailjs-com';
+import { Helmet } from 'react-helmet';
 
 function Contact() {
+  
+  // use emailjs to send submissions to Sankofa
+  function sendEmail(e) {
+    e.preventDefault();
+  
+  emailjs.sendForm('service_irjhyda', 'template_kjqp788', e.target, 'user_jLJrfhSZnLUR4RuRvjcbw')
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+
+    e.target.reset()
+  }
+
   return (
     <div className="container">
+      <Helmet>
+        <title>Sankofa | Contact Us</title>
+        <style>{'body { background-color: #FFFCF8; }'}</style>
+      </Helmet>
+      
       <header className="header">
         <h1 className="contact-us"> Contact Us </h1>
       </header>
@@ -15,7 +37,7 @@ function Contact() {
           
           <div className="email-contact">
             <img src={mail} className="icon" alt="Mail"/>
-            <p className="contact-text">Jasmin Burns: jlsheppard@ucdavis.edu</p>
+            <p className="contact-text">Jazmin Burns: jlsheppard@ucdavis.edu</p>
           </div>
 
           <br></br>
@@ -53,14 +75,13 @@ function Contact() {
         <div class='fcf-body'>
           
           <div id="fcf-form">
-            <form id="fcf-form-id" class="fcf-form-class" method="post" 
-            action="contact-form-process.php"></form>
+            <form id="fcf-form-id" class="fcf-form-class" onSubmit={sendEmail}>
 
             <div class="same-row">
               <div class="fcf-form-group name-box">
                 <label for="Name" class="fcf-label">Name</label>
                 <div class="fcf-input-group name-css">
-                  <input type="text" id="Name" name="Name" class="fcf-form-control" required></input>
+                  <input type="text" name="name" class="fcf-form-control" required></input>
                 </div>
               </div>
 
@@ -69,7 +90,7 @@ function Contact() {
               <div class="fcf-form-group email-box">
                 <label for="Email" class="fcf-label">Email address</label>
                 <div class="fcf-input-group email-css">
-                    <input type="email" id="Email" name="Email" class="fcf-form-control" required></input>
+                    <input type="email" name="email" class="fcf-form-control" required></input>
                 </div>
               </div>
             </div>
@@ -80,7 +101,7 @@ function Contact() {
             <div class="fcf-form-group">
               <label for="Subject" class="fcf-label">Subject</label>
               <div class="fcf-input-group">
-                  <input type="text" id="Email" name="Email" class="fcf-form-control" required></input>
+                  <input type="text" name="subject" class="fcf-form-control" required></input>
               </div>
             </div>
 
@@ -89,7 +110,7 @@ function Contact() {
             <div class="fcf-form-group">
               <label for="Message" class="fcf-label">Write your message here:</label>
               <div class="fcf-input-group">
-                  <input type="text" id="Email" name="Email" class="fcf-form-control-thick" required></input>
+                  <textarea name="message" class="fcf-form-control-thick" required></textarea>
               </div>
             </div>
 
@@ -103,7 +124,7 @@ function Contact() {
             </div>
 
             <br></br>
-
+            </form>
           </div>
         </div>
 
@@ -114,13 +135,13 @@ function Contact() {
           <div class="box-area"></div>
           
           <div class="name-and-email" id="fcf-form">
-            <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-us-form.php">
+            <form id="fcf-form-id" class="fcf-form-class" onSubmit={sendEmail}>
               
               <div class="same-row">
                 <div class="fcf-form-group name-box">
                   <label for="Name" class="fcf-label">Name</label>
-                  <div class="fcf-input-group name-css">
-                    <input type="text" id="Name" name="Name" class="fcf-form-control" required></input>
+                  <div class="fcf-input-group nam e-css">
+                    <input type="text" name="name" class="fcf-form-control" required></input>
                   </div>
                 </div>
 
@@ -129,7 +150,7 @@ function Contact() {
                 <div class="fcf-form-group email-box">
                   <label for="Email" class="fcf-label" id="email-box">Email address</label>
                   <div class="fcf-input-group email-css">
-                    <input type="email" id="Name" name="Name" class="fcf-form-control" required></input>
+                    <input type="email" name="email" class="fcf-form-control" required></input>
                   </div>
                 </div>
               </div>
