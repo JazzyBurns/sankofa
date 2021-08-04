effa4b0d65bcd0f2374deca7a103ff995b23ca86
JazzyBurns <79717295+JazzyBurns@users.noreply.github.com>
2021-08-04 15:56:54 -0700

    Update Materials.js

--- src/components/RecordedSessions/Materials.js
+++ src/components/RecordedSessions/Materials.js
@@ -1,7 +1,7 @@
 // example import: "import INSERTNAME from './Thumbnails/fileName.png/jpg';"
 import presentationImg from './Thumbnails/sankofapresentation.png';
 import recordingImg from './Thumbnails/recordedsession.png';
-import recordingImg from './Thumbnails/SafeBlackSpaceimg.JPG';
+import recordingImg2 from './Thumbnails/SafeBlackSpaceimg.JPG';
 
 export const PresentationData = [
   /* EXAMPLE:
@@ -24,7 +24,12 @@ export const RecordingData = [
     thumbnail: recordingImg,
     url: "https://ucdavis.box.com/s/ljlz8mfb4hcsfh19zi5kfqr9i4ulb2cw"
   },
-  
+  */
+  {
+    title: "Safe Black Space Presentation",
+    thumbnail: recordingImg2,
+    url: "https://ucdavis.app.box.com/file/830938210924"
+  }
 ]
 
 export default {
